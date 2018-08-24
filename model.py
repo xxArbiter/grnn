225f6eac82cbaebd6cf2c7f8df081672ec5cd4bc
xxArbiter <tju.arbiter@gmail.com>
2018-08-24 21:03:20 +0800

    change propogator

--- model.py
+++ model.py
@@ -58,25 +58,40 @@ class Propogator(nn.Module):
         self.nNode = opt.nNode              # n
         self.dimFeature = opt.dimFeature    # d
         self.dimHidden = opt.dimHidden      # D
-
-        for i in range(self.nNode):
-            cell = gruCell(opt)
-            self.add_module("gruCell_{}".format(i), cell)
-
-        self.cells = AttrProxy(self, "gruCell_")
+        
+        self.resetGate = nn.Sequential(
+            nn.Linear(self.dimHidden + self.dimFeature, self.dimHidden),
+            nn.Sigmoid()
+        )
+        self.updateGate = nn.Sequential(
+            nn.Linear(self.dimHidden + self.dimFeature, self.dimHidden),
+            nn.Sigmoid()
+        )
+        self.transform = nn.Sequential(
+            nn.Linear(self.dimHidden + self.dimFeature, self.dimHidden),
+            nn.Tanh()
+        )
+        self.output = nn.Linear(self.dimHidden, self.dimFeature)
 
     def forward(self, x, hState, A):
-        O = torch.zeros(self.batchSize, self.nNode, self.dimFeature).double()
-        H = torch.zeros(self.batchSize, self.dimHidden, self.nNode).double()
+        """
+        x: [b, n, d]
+        h: [b, D, n]
+        """
+        S = torch.bmm(hState, A)            # [b, D, n]
+        S = S.transpose(1, 2)               # [b, n, D]
 
-        S = torch.bmm(hState, A)
+        i = torch.cat((S, x), 2)            # [b, n, D + d]
+        z = self.resetGate(i)
+        r = self.updateGate(i)
+        jointI = torch.cat((r * S, x), 2)
+        hHat = self.transform(jointI)
+        h = (1 - z) * S + z * hHat          # [b, n, D]
+        o =  self.output(h)                 # [b, n, 1]
 
-        for n in range(self.nNode):
-            O[:, n, :], H[:, :, n] = self.cells[n](x[:, n, :], S[:, :, n])
+        hState = h.transpose(1, 2)
 
-        hState = H
-
-        return O, hState
+        return o, hState
 
 
 class GRNN(nn.Module):
@@ -94,7 +109,7 @@ class GRNN(nn.Module):
         """
         x: input node features [batchSize, interval, nNode, dimFeature]
         hState: hidden state [batchSize, dimHidden, nNode]
-        A: transfer matrix [nNode, nNode]
+        A: transfer matrix [batchSize, nNode, nNode]
         """
         O = torch.zeros(self.batchSize, self.interval, self.nNode, self.dimFeature).double()
 
