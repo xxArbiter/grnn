3473d108d24d94b90cd7efddf4cc22b698484fe1
xxArbiter <tju.arbiter@gmail.com>
2018-08-26 22:10:51 +0800

    slight change

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import datetime
 import torch
+import os
 import scipy.io as spio
 
 def getTime(begin, end=datetime.datetime.now()):
@@ -17,4 +18,7 @@ class Log(object):
         print('[Log] %d iteration. MSELoss: %.4f, Train used: %s.' % (t + 1, self.mseLoss[t], getTime(start)))
 
     def saveResult(self):
+        if not os.path.exists('result'):
+            os.mkdir('result')
+            
         spio.savemat('result/result_%d.mat' % self.taskID, {'prediction': self.prediction, 'mseLoss': self.mseLoss})
