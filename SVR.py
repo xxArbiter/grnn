b99bff1428960205973733e6e142cb9b4a7d7cfc
xxArbiter <tju.arbiter@gmail.com>
2018-09-02 17:19:39 +0800

    add svr & gbdt

---
+++ SVR.py
@@ -0,0 +1,37 @@
+import numpy as np
+import time
+from sklearn.metrics import mean_squared_error
+from sklearn import svm
+from dataset import trafficDataLoader
+import matplotlib.pyplot as plt
+import scipy.io as spio
+
+taskID = 1
+
+dataLoader = trafficDataLoader(taskID)
+data = dataLoader.data[0, :, :, 0].transpose()  # [n, T]
+data /= 100
+
+X = []
+Y = []
+for t in range(data.shape[1] - 144):    # T - truncate
+    X.append(data[:, t:t+144])          # annotation[n, 144]
+    Y.append(data[:, t+144])            # target[n, 1]
+
+trainX = np.array(X[:-720])             # [train, n, 144]
+trainY = np.array(Y[:-720])            # [train, n, 1]
+testX = np.array(X[-720:])             # [test, n, 144]
+
+start = time.clock()
+prediction = np.zeros((data.shape[0], 720))
+y = data[:, -720:]
+for n in range(data.shape[0]):
+    svr = svm.SVR()
+    svr.fit(trainX[:, n, :], np.reshape(trainY[:, n, 0], trainY.shape[0]))
+    prediction[n, :] = svr.predict(testX)
+    print('Finish %d nodes, MSE: used: %.1fs' % (n+1, mean_squared_error(y[n, :], prediction[n, :]), time.clock() - start))
+
+error = y - prediction
+MSE = (error**2).mean()
+VD = ((error - error.mean())**2).mean()
+print('MSE: %.4f, VD: %.4f, used: %.1fs' % (MSE, VD, time.clock() - start))
--- ha.py
+++ ha.py
@@ -5,10 +5,13 @@ from dataset import trafficDataLoader
 taskID = 1
 
 dataLoader = trafficDataLoader(taskID)
-data = dataLoader.data  # [n, T]
+data = dataLoader.data[0, :, :, 0].transpose()  # [n, T]
 
 n, T = data.shape
 x = data[:, T - 28*144: T - 7*144]
 y = (x[:, :7*144] + x[:, 7*144:14*144] + x[:, 14*144:]) / 3
-MSE = ((y - data[:, T - 7*144:])**2).mean()
-print(MSE)
+
+error = y - data[:, T - 7*144:]
+MSE = (error**2).mean()
+VD = ((error - error.mean())**2).mean()
+print('MSE: %.4f, VD: %.4f' % (MSE, VD))
