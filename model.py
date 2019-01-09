b0364cef1c49a2b783066bc4d5a82bd21bfe4543
Vrezh Khalatyan <vkhalat@calstatela.edu>
2019-01-08 21:12:35 -0800

    Update Import Comments

--- model.py
+++ model.py
@@ -1,4 +1,12 @@
+"""PyTorch is a Python-based scientific computing package 
+that uses the power of graphics processing units. 
+It is also one of the preferred deep learning research platforms 
+built to provide maximum flexibility and speed. It is known for 
+providing two of the most high-level features; namely, tensor 
+computations with strong GPU acceleration support and building 
+deep neural networks on a tape-based autograd systems."""
 import torch
+"""A kind of Tensor that is to be considered a module parameter."""
 import torch.nn as nn
 
 class AttrProxy(object):
